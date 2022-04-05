41fe4324db6dfc059d5428a6bde145f505727aaf
muhammadmb <muhammadmagdyje@gmail.com>
2022-04-05 22:28:22 +0200

    implement insertBook and editBook functions

--- src/Store/BooksSlice.js
+++ src/Store/BooksSlice.js
@@ -7,7 +7,10 @@ const initialState = {
     books: [],
     errors: "",
     booksLoading: false,
-    headers: ""
+    headers: "",
+    bookProccess: false,
+    bookUpdate: null,
+    bookAdd: null
 }
 
 export const getBooks = createAsyncThunk(
@@ -27,6 +30,69 @@ export const getBooks = createAsyncThunk(
     }
 )
 
+export const insertBook = createAsyncThunk(
+    "books/insert",
+    async (bookData, thunkAPI) => {
+        const { rejectWithValue } = thunkAPI;
+        const { genreId } = bookData;
+        if (Math.floor((new Date()).getTime() / 1000) > ParseJWT(JSON.parse(localStorage.getItem("user")).token).exp) {
+            await thunkAPI.dispatch(refreshToken());
+        }
+        try {
+            const books = await axios.post(`${API_URL}/Genres/${genreId}/Books`,
+                bookData,
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${JSON.parse(localStorage.getItem("user"))?.token}`
+                    }
+                })
+                .catch(
+                    function (error) {
+                        if (error.response) {
+                            return (error.response)
+                        } else if (error.request) {
+                            return (error.request)
+                        } else {
+                            return (error.message)
+                        }
+                    });
+            return books;
+        }
+        catch (error) {
+            return rejectWithValue(error.message);
+        }
+    }
+)
+
+export const editBook = createAsyncThunk(
+    "books/edit",
+    async (bookData, thunkAPI) => {
+        const { id, genreId } = bookData;
+        if (Math.floor((new Date()).getTime() / 1000) > ParseJWT(JSON.parse(localStorage.getItem("user")).token).exp) {
+            await thunkAPI.dispatch(refreshToken());
+        }
+        const book = await axios.put(`${API_URL}/Genres/${genreId}/Books/${id}`,
+            bookData,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${JSON.parse(localStorage.getItem("user"))?.token}`
+                }
+            }).catch(
+                function (error) {
+                    if (error.response) {
+                        return (error.response)
+                    } else if (error.request) {
+                        return (error.request)
+                    } else {
+                        return (error.message)
+                    }
+                });
+        return book;
+    }
+)
+
 export const deleteBook = createAsyncThunk(
     "books/deleteBook",
     async (bookData, thunkAPI) => {
@@ -35,23 +101,30 @@ export const deleteBook = createAsyncThunk(
         const { bookId, genreId } = bookData;
 
         if (Math.floor((new Date()).getTime() / 1000) > ParseJWT(JSON.parse(localStorage.getItem("user")).token).exp) {
-            thunkAPI.dispatch(refreshToken());
+            await thunkAPI.dispatch(refreshToken());
         }
-        setTimeout(async () => {
-            try {
-                const books = await axios.delete(`${API_URL}/Genres/${genreId}/Books/${bookId}`,
-                    {
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization': `Bearer ${JSON.parse(localStorage.getItem("user"))?.token}`
+        try {
+            const books = await axios.delete(`${API_URL}/Genres/${genreId}/Books/${bookId}`,
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${JSON.parse(localStorage.getItem("user"))?.token}`
+                    }
+                }).catch(
+                    function (error) {
+                        if (error.response) {
+                            return (error.response)
+                        } else if (error.request) {
+                            return (error.request)
+                        } else {
+                            return (error.message)
                         }
                     });
-                return books.data;
-            }
-            catch (error) {
-                return rejectWithValue(error.message);
-            }
-        }, 200);
+            return books.data;
+        }
+        catch (error) {
+            return rejectWithValue(error.message);
+        }
     }
 )
 
@@ -72,6 +145,46 @@ const BooksSlice = createSlice({
         [getBooks.rejected]: (state, action) => {
             state.booksLoading = false;
             state.errors = action.payload;
+        },
+
+        [insertBook.pending]: (state, action) => {
+            state.errors = null;
+            state.bookProccess = true;
+            state.bookAdd = false;
+            state.bookUpdate = false;
+        },
+        [insertBook.fulfilled]: (state, action) => {
+            if (action.payload.status === 201) {
+                state.bookProccess = false;
+                state.bookAdd = true;
+            } else {
+                state.errors = action.payload?.data?.errors?.description[0];
+                state.bookProccess = false;
+            }
+        },
+        [insertBook.rejected]: (state, action) => {
+            state.errors = action.payload;
+            state.bookProccess = false;
+        },
+
+        [editBook.pending]: (state, action) => {
+            state.errors = null;
+            state.bookProccess = true;
+            state.bookAdd = false;
+            state.bookUpdate = false;
+        },
+        [editBook.fulfilled]: (state, action) => {
+            if (action.payload.status === 204) {
+                state.bookProccess = false;
+                state.bookUpdate = true;
+            } else {
+                state.errors = action.payload?.data?.errors?.description[0];
+                state.bookProccess = false;
+            }
+        },
+        [editBook.rejected]: (state, action) => {
+            state.errors = action.payload;
+            state.bookProccess = false;
         },
 
         [deleteBook.pending]: (state, action) => {
