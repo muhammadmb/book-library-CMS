1a0c6c00b18ee228d6f6818bc7ecaa018239315b
muhammadmb <muhammadmagdyje@gmail.com>
2022-04-21 23:17:24 +0200

    Add Feedback card

--- src/Components/FeedbackCard/FeedbackCard.js
+++ src/Components/FeedbackCard/FeedbackCard.js
@@ -1,81 +1,99 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./FeedbackCardStyle.css";
 import LocalPostOfficeIcon from "@material-ui/icons/LocalPostOffice";
 import DraftsIcon from "@material-ui/icons/Drafts";
 import CloseIcon from "@material-ui/icons/Close";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteFeedback, editFeedback } from "../../Store/FeedbackSlice";
 
-const FeedbackCard = (props) => {
-  const [readed, setReaded] = useState(props.read);
-  const [readMore, setReadMore] = useState(props.details.length <= 150);
-  const isNew =
-    props.date.getFullYear() === new Date().getFullYear() &&
-    props.date.getMonth() === new Date().getMonth() &&
-    props.date.getDate() === new Date().getDate();
+const FeedbackCard = ({ feedback }) => {
+  const [readed, setReaded] = useState(feedback.isReaded);
+  const [readMore, setReadMore] = useState(feedback.description?.length < 150);
+  const { feedbacks } = useSelector((state) => state.feedbacks);
+  const isNew = (((new Date()).getTime() - new Date(feedback.addedDate).getTime()) / (1000 * 3600 * 24) < 7);
+  const dispatch = useDispatch();
 
-  const handleReaded = (id) => {
+  const handleReaded = () => {
     setReaded(!readed);
-    props.handelRead(id, readed);
   };
 
-  const handelDelete = (id) => {
-    props.handelDelete(id);
+  useEffect(() => {
+    dispatch(editFeedback({
+      id: feedback.id,
+      path: "isReaded",
+      value: readed
+    }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [readed]);
+
+  const handelDelete = () => {
+    dispatch(deleteFeedback(feedback.id));
   };
 
-  const handelReadMore = (id) => {
+  const handelReadMore = () => {
     setReadMore(true);
   };
 
   return (
-    <>
-      <div className={readed ? "info-card read" : "info-card"}>
-        <div className="status">
-          <h3>{props.email}</h3>
-          <div className="readed">
-            {readed ? (
-              <DraftsIcon
-                titleAccess="mark as unread"
-                className="icon"
-                onClick={() => handleReaded(props.id)}
-              />
-            ) : (
-              <>
-                {isNew ? <span className="new">NEW</span> : null}
-
-                <LocalPostOfficeIcon
-                  titleAccess="mark as read"
+    <div
+      className={readed ? "info-card read" : "info-card"}
+    >
+      <div className="status">
+        <h3>{feedback.email}</h3>
+        <div className="readed">
+          {
+            readed ?
+              (
+                <DraftsIcon
+                  titleAccess="mark as unread"
                   className="icon"
-                  onClick={() => handleReaded(props.id)}
+                  onClick={handleReaded}
                 />
-              </>
-            )}
-            <CloseIcon
-              titleAccess="delete message"
-              className="delete-icon"
-              onClick={() => handelDelete(props.id)}
-            />
-          </div>
+              )
+              :
+              (
+                <>
+                  {
+                    isNew &&
+                    <span className="new">NEW</span>
+                  }
+                  <LocalPostOfficeIcon
+                    titleAccess="mark as read"
+                    className="icon"
+                    onClick={handleReaded}
+                  />
+                </>
+              )
+          }
+          <CloseIcon
+            titleAccess="delete message"
+            className="delete-icon"
+            onClick={handelDelete}
+          />
         </div>
-
-        <p className="details">
-          {readMore ? (
-            props.details
-          ) : (
-            <>
-              {JSON.stringify(props.details).substring(1, 150) + "... "}
-              <span
-                className="read-more"
-                onClick={() => handelReadMore(props.id)}
-              >
-                READ MORE
-              </span>
-            </>
-          )}
-        </p>
-        <span className="date">
-          {JSON.stringify(props.date).substring(3, 11)}
-        </span>
       </div>
-    </>
+
+      <p className="details">
+        {
+          readMore ? (feedback.description)
+            :
+            (
+              <>
+                {JSON.stringify(feedback.description).substring(1, 150) + "... "}
+                <span
+                  className="read-more"
+                  onClick={handelReadMore}
+                >
+                  READ MORE
+                </span>
+              </>
+            )
+        }
+      </p>
+      <span className="date">
+        {JSON.stringify(feedback.addedDate).substring(3, 11)}
+      </span>
+    </div>
   );
 };
 
