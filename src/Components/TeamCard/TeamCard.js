ab0069817e6e8674f13f2c9ceee00518bce71285
muhammadmb <muhammadmagdyje@gmail.com>
2022-04-29 00:48:48 +0200

    Implement Team Card

--- src/Components/TeamCard/TeamCard.js
+++ src/Components/TeamCard/TeamCard.js
@@ -1,69 +1,160 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { GetAge } from '../../Helper/GetAge';
 import './TeamCardStyle.css';
+import maleAvatar from '../../Images/male-avatar.png';
+import femaleAvatar from '../../Images/female-avatar.png';
+import { addUserToRole, deleteUser, removeUserFromRole } from '../../Store/UsersSlice';
+import LoadingAnimation from '../../Loading/LoadingAnimation/LoadingAnimation';
 
-const TeamCard = () => {
+const TeamCard = ({ user }) => {
 
     const [options, setOptions] = useState(false);
-    const [rolls, setRolls] = useState(false);
+    const [roleOptions, setRoleOptions] = useState(false);
+    const [deleting, setDeleting] = useState(false);
+    const { userDeleting } = useSelector((state => state.users));
+    const { appRoles } = useSelector((state) => state.roles);
+    const [userRole, setUserRole] = useState(user.roles);
+    const dispatch = useDispatch();
 
-    return (
-        <div className='team-card-container'>
-            <div>
-                <div className="main-details">
-                    <img
-                        src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
-                        alt="profile"
-                    />
-                    <div>
-                        <h3>Muhammad</h3>
-                        <span>28 yrs, Male</span>
-                    </div>
+    const handleRoleChange = (role) => {
+        if (userRole.includes(role)) {
+            setUserRole(userRole.filter(r => r !== role));
+            dispatch(removeUserFromRole({
+                role,
+                email: user.email
+            }));
+        } else {
+            setUserRole(userRole => [...userRole, role]);
+            dispatch(addUserToRole({
+                role,
+                email: user.email
+            }));
+        }
+    }
 
-                </div>
-                <i
-                    className="fa fa-ellipsis-v"
-                    aria-hidden="true"
-                    onMouseEnter={() => setOptions(true)}
-                    onMouseLeave={() => setOptions(false)}
-                ></i>
+    const handleDelete = (email) => {
+        dispatch(deleteUser(email))
+        setDeleting(true);
+    }
+
+    const UserAge = () => {
+        return (
+            <span>
+                {
+                    user.dateOfBirth.substr(0, 4) !== "0001" &&
+                    <>
+                        {
+                            GetAge(new Date(user.dateOfBirth.substr(0, 10)))
+                        }
+                        <> yrs, </>
+                    </>
+                }
+                {user.gender}
+            </span>
+        )
+    }
+
+    const UserOptions = () => {
+        return (
+            <>
                 {
                     options &&
                     <ul className='drop-down'
                         onMouseEnter={() => setOptions(true)}
                         onMouseLeave={() => setOptions(false)}
                     >
-                        {!rolls &&
+                        {!roleOptions ?
                             <>
-                                <li>Delete</li>
-                                <li onClick={() => setRolls(true)} >Role {'>'}</li>
+                                <li onClick={() => handleDelete(user.email)} >Delete</li>
+                                <li onClick={() => setRoleOptions(true)} >Role {'>'}</li>
+                            </>
+                            :
+                            <>
+                                <li onClick={() => setRoleOptions(false)} >{'<'}</li>
+                                {
+                                    appRoles.map((role) => (
+                                        <li
+                                            key={role.id}
+                                            className=
+                                            {
+                                                userRole.includes(role.name) ?
+                                                    "selected"
+                                                    :
+                                                    ""
+                                            }
+                                            onClick={() => handleRoleChange(role.name)}
+                                        >
+                                            {role.name}
+                                        </li>
+                                    ))
+                                }
+
                             </>
                         }
-
-                        {rolls &&
-                            <>
-                                <li onClick={() => setRolls(false)} >{'<'}</li>
-                                <li>Master</li>
-                                <li>Admin</li>
-                                <li>Editor</li>
-                            </>
-                        }
-
                     </ul>
                 }
-            </div>
+            </>
+        )
+    }
 
-            <div>
-                <span className='phone'>+31 0255 012 0110</span>
-                <span>contact</span>
-            </div>
+    return (
+        <>
+            {
+                (userDeleting && deleting) ?
+                    <LoadingAnimation />
+                    :
+                    <div className='team-card-container'>
+                        <div>
+                            <div className="main-details">
 
+                                <img
+                                    src={
+                                        user.pictureUrl ?
+                                            user.pictureUrl
+                                            :
+                                            user.gender.toLowerCase() === "male" ?
+                                                maleAvatar
+                                                :
+                                                femaleAvatar
+                                    }
+                                    alt="profile"
+                                />
 
-            <div>
-                <span>3 Feb 2017</span>
-                <span>Editor</span>
-            </div>
+                                <div>
+                                    <h3>{user.firstName}</h3>
+                                    <UserAge />
+                                </div>
+                            </div>
 
-        </div>
+                            <i
+                                className="fa fa-ellipsis-v"
+                                aria-hidden="true"
+                                onMouseEnter={() => setOptions(true)}
+                                onMouseLeave={() => setOptions(false)}
+                            ></i>
+                            <UserOptions />
+
+                        </div>
+
+                        <div>
+                            {
+                                user.phoneNumber &&
+                                <>
+                                    <span className='phone'>{user.phoneNumber}</span>
+                                    <span>{user.email}</span>
+                                </>
+                            }
+                        </div>
+
+                        <div>
+                            <span>{user.addedDate.substr(0, 10)}</span>
+                            <span>{userRole.toString().replace(",", ", ")}</span>
+                        </div>
+                    </div>
+            }
+
+        </>
     )
 }
 
